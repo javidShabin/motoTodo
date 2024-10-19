eb8536d41463b8ded3311b672a398c5a6acd3f69
javid shabin <javid.main.act@gmail.com>
2024-10-20 00:13:01 +0530

    make update

--- routes/v1/taskRoter.js
+++ routes/v1/taskRoter.js
@@ -10,7 +10,7 @@ const router = express.Router(); // Correctly initialize the router
 
 // Define routes
 router.post("/addItems", addItems);
-router.get("getItemById", getItemById);
+router.get("/getItemById/:id", getItemById);
 router.get("/getAllItems", getAllItems);
 router.put("/editItem/:id", editItem); // Added :id to handle the parameter
 router.delete("/deleteItem/:id", deleteItem); // Added :id to handle the parameter
