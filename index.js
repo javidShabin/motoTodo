4a504494e035bb389b5f9af8bc3bfc16c7a3e12e
javid shabin <javid.main.act@gmail.com>
2024-12-05 23:24:06 +0530

    updated

--- index.js
+++ index.js
@@ -1,27 +1,32 @@
-
 const express = require("express");
 const app = express();
-const cookieParser = require('cookie-parser');
+const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const { dbconnection } = require("./config/dbConnection");
-const { apiRouter } = require('./routes');
+const { apiRouter } = require("./routes");
 const port = 2000;
 
 // Middleware for CORS
-app.use(cors({
-  credentials: true,
-  origin: "http://localhost:5173"
-}));
+app.use(
+  cors({
+    credentials: true,
+    origin: "https://todo-frontend-alpha-ashen.vercel.app",
+  })
+);
 
 // Middleware for cookies and JSON parsing
 app.use(cookieParser());
 app.use(express.json());
 
 // API Routes
-app.use('/api', apiRouter);
+app.use("/api", apiRouter);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 // MongoDB Database Connection
-dbconnection()
+dbconnection();
 
 // Start the Express Server
 app.listen(port, () => {
